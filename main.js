fd1fc741fe4f21ee58f6673fe031331f24c90e0b
Ryan Helm <rjhelm@github.io>
2021-04-09 21:56:32 -0600

    added to javascript code

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-function fetchIssue() {
-    let issues = JSON.parse(localStorage.getItem('issues'));
-    let issuesList = document.getElementById('issuesList');
-
-    issuesList = '';
-    for (let i = 0; i < issues.length; i++) {
-        let id = issues[i].id;
-        let desc = issues[i].severity;
-        let assignedTo = issues[i].assignedTo;
-        let status = issues[i].status;
-
-        issuesList.innerHTML +=   '<div class="well">'+
-                              '<h6>Issue ID: ' + id + '</h6>'+
-                              '<p><span class="label label-info">' + status + '</span></p>'+
-                              '<h3>' + desc + '</h3>'+
-                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' '+
-                              '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
-                              '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> '+
-                              '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
-                              '</div>';
-  }
-}  
+function fetchIssues () {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+    
+    issuesList.innerHTML = '';
+    
+    for (var i = 0; i < issues.length; i++) {
+      var id = issues[i].id;
+      var desc = issues[i].description;
+      var severity = issues[i].severity;
+      var assignedTo = issues[i].assignedTo;
+      var status = issues[i].status;
+      
+      issuesList.innerHTML +=   '<div class="well">'+
+                                '<h6>Issue ID: ' + id + '</h6>'+
+                                '<p><span class="label label-info">' + status + '</span></p>'+
+                                '<h3>' + desc + '</h3>'+
+                                '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' '+
+                                '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                                '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> '+
+                                '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
+                                '</div>';
     }
-}
+  }
+
+  document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+  function saveIssues() {
+      let issueId = chance.guid();
+      let issueDesc = document.getElementById('issueDescInput').value;
+      let issueSeverity = document.getElementById('issueSeverityInput').value;
+      let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+      let issueStatus = 'Open';
+  }
